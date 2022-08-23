050f133ef700a3c19cb19c5087123cb0fb1ffb15
luisniell1 <108769235+luisniell1@users.noreply.github.com>
2022-08-23 14:45:45 -0300

    Add files via upload

---
+++ Puertos_abiertos.py
@@ -0,0 +1,23 @@
+import sys
+import socket
+
+objetivo = socket.gethostbyname(input("Inserte la direccion IP:  "))
+
+print("Escaneando objetivo " + objetivo)
+
+try:
+    for port in range(1,150):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        socket.setdefaulttimeout(1)
+        resultado = s.connect_ex((objetivo, port))
+        if resultado == 0:
+            print("El puerto {} esta abierto".format(port))
+        s.close()
+except:
+    print("\n Saliendo...")
+    sys.exit(0)
+    
+                          
+    
+
+
